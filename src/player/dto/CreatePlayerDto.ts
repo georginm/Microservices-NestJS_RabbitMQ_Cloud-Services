3f685cafa30e514371677c25d1b90e2c65b81d7e
georginm <georginmatheus@gmail.com>
2021-11-03 16:02:45 -0300

    first commit

---
+++ src/player/dto/CreatePlayerDto.ts
@@ -0,0 +1,5 @@
+export class CreatePlayerDto {
+  readonly _id: string;
+  readonly email: string;
+  readonly name: string;
+}
---
+++ src/player/interfaces/IPlayer.ts
@@ -0,0 +1,9 @@
+export interface IPlayer {
+  readonly _id: string;
+  readonly email: string;
+  phoneNumber: string;
+  name: string;
+  ranking: string;
+  rankingPosition: number;
+  avatar_url: string;
+}
